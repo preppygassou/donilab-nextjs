d3b9f6170c012fd612ff79035da8bc766241593e
Preppy <ninopreppy@gmail.com>
2024-10-22 14:46:57 -0300

    last

--- app/(site)/site/[siteId]/programs/page.tsx
+++ app/(site)/site/[siteId]/programs/page.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { PostTableView } from '@/components/tables/post-tables/post-table';
+import { ProgramTableView } from '@/components/tables/program-tables/program-table';
 
 const Page = (params: { siteId: string }) => {
   const data= [{title:"fkf"}]
   return (
     <>
       <div className="flex-1 space-y-4  p-4 pt-6 md:p-8">
-        <PostTableView data={data} />
+        <ProgramTableView data={data} />
       </div>
     </>
   )
